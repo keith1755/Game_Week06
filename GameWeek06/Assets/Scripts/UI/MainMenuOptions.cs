d94311b142ec749961a58983a8b9e0b11d23968a
keith1755 <keith1755@gmail.com>
2021-11-09 17:18:13 +1100

    collectables import

---
+++ GameWeek06/Assets/Scripts/UI/MainMenuOptions.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuOptions : MonoBehaviour
+{
+
+    public static bool isLoading = false;
+
+    public void NewGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void LoadGame()
+    {
+        isLoading = true;
+        SceneManager.LoadScene(1);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+        Debug.Log("Game is exiting");
+        //Just to make sure its working
+    }
+}
